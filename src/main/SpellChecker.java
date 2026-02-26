6f6b0fb347ff33c8f0dfd187ce5f3fc62a5d2f01
calvinkapral <ckapral@outlook.com>
2026-02-25 19:56:57 -0600

    Add spell checker stub

---
+++ src/main/SpellChecker.java
@@ -0,0 +1,18 @@
+public class SpellChecker {
+
+    public SpellChecker() {}
+
+    public int size() {
+        return 0;
+    }
+
+    public void addWord(String word) {}
+
+    public boolean isCorrect(String word) {
+        return false;
+    }
+
+    public String suggest(String word) {
+        return null;
+    }
+}
